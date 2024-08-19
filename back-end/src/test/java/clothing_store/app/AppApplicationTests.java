d68192fcddbd6e7c9dcd6b8365349b6f5ddb6148
João Pedro Graciotim <graciotim92@gmail.com>
2024-08-19 20:38:30 -0300

    Add files via upload

---
+++ back-end/src/test/java/clothing_store/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package clothing_store.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
