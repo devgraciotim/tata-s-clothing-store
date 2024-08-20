1d1fd1963aea9e4e479750abf3fb4d5b4fe2d3d1
João Graciotim <graciotim92@gmail.com>
2024-08-19 21:01:11 -0300

    create initial Entities

---
+++ back-end/src/main/java/clothing_store/app/entity/Client.java
@@ -0,0 +1,25 @@
+package clothing_store.app.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String name;
+    String cpf;
+    Integer age;
+    String phone;
+}
---
+++ back-end/src/main/java/clothing_store/app/entity/Product.java
@@ -0,0 +1,23 @@
+package clothing_store.app.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String name;
+    Double price;
+}
